40fea5c158287ff772b65bf5f3aa5a0ae4a4bfc6
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-08-03 16:26:12 -0700

    chore: set environment.

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,29 @@
-var path = require('path')
+let path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+let bodyParser = require('body-parser')
+let cors = require('cors')
+
+dotenv.config();
+console.log(`Your API key is ${process.env.API_KEY}`);
+
+// Call Sentiment Analysis
+// let textapi = new meaningCloud({
+//    application_key: process.env.API_KEY
+// });
 
 const app = express()
+
+app.use(cors())
+
+// to use json
+app.use(bodyParser.json())
+
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
@@ -10,14 +31,14 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('/dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    res.json(mockAPIResponse)
 })
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,10 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {	
+        libraryTarget: 'var',	
+        library: 'Client'	
+    },
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
@@ -14,7 +18,11 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }        
         ]
     },
     plugins: [
