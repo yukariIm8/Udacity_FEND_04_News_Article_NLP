e2b713cc689158718c68bbaf55dbbd48f796dd62
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-08-06 10:31:53 -0700

    feat: API call to meaning cloud. chore: set up webpack.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,13 +4,28 @@ function handleSubmit(event) {
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
+    console.log(formText);
 
+    // POST request to express server.
+    fetch('http://localhost:3000/analyzeTxt', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name: formText}), // body data type must match "Content-Type" header
+    })
+    .then((res) => {
+        document.getElementById('results').innerHTML = res.subjectivity
+    })
+}
+    /*
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
     })
-}
+    */
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -2,11 +2,11 @@ let path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const dotenv = require('dotenv');
+const fetch = require("node-fetch");
 let bodyParser = require('body-parser')
 let cors = require('cors')
 
 dotenv.config();
-console.log(`Your API key is ${process.env.API_KEY}`);
 
 // Call Sentiment Analysis
 // let textapi = new meaningCloud({
@@ -35,10 +35,62 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+app.listen(3000, function () {
+    console.log('NLP app listening on port 3000!')
 })
 
+/*
 app.get('/test', function (req, res) {
     res.json(mockAPIResponse)
 })
+*/
+
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=';
+const apiKey = process.env.API_KEY
+const output = '&of=json';
+const text = '&txt=';
+const model = '&model=general';
+const lang = '&lang=en'; 
+
+const analyzeTxt = async (req, res) => {
+    let txt = req.body.name;
+    console.log(txt)
+    console.log(baseURL+apiKey+output+text+txt+model+lang);
+    const resultPromise = await fetch(baseURL+apiKey+output+text+txt+model+lang)
+    .then((response) => { // Extract JSON payload.
+        return response.json();
+    })
+    .then((json) => {
+        console.log(json);
+    })
+    .then((data) => {
+        res.send(data);
+    })
+    .catch((error) => {
+        console.log('something went wrong: ', error.message);
+    })
+};
+
+// const analyzeTxt = async (req, res) => {
+//     let txt = req.body.name;
+//     console.log(txt)
+//     console.log(baseURL+apiKey+output+text+txt+model+lang);
+//     const resultPromise = await fetch(baseURL+apiKey+output+text+txt+model+lang)
+//     .then((response) => { // Extract JSON payload.
+//         return response.json();
+//     })
+//     .then((json) => {
+//         console.log(json);
+//     })
+//     .catch((error) => {
+//         console.log('something went wrong: ', error.message);
+//     })
+// };
+
+app.post('/analyzeTxt', analyzeTxt);
+
+/*
+app.post('/analyzeTxt', function (req, res) {
+    res.send(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&of=json&txt=${formTxt}&model=general&lang=en`)
+})
+*/
