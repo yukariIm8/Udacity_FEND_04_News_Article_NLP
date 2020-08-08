fa1faabae0ab99971828739aa31bd69bbb67f327
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-08-08 02:07:34 -0700

    feat: show text snippet.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,9 +1,10 @@
 const updateUI = (data) => {
-    document.getElementById('score').innerHTML = 'Score: ' + data.score_tag
+    document.getElementById('score').innerHTML = 'Polarity (P: positive/NEU: neutral/N: negative): ' + data.score_tag
     document.getElementById('agreement').innerHTML = 'Agreement: ' + data.agreement
     document.getElementById('subjectivity').innerHTML = 'Subjectivity: ' + data.subjectivity
     document.getElementById('confidence').innerHTML = 'Confidence: ' + data.confidence
     document.getElementById('irony').innerHTML = 'Irony: ' + data.irony
+    document.getElementById('text-snippet').innerHTML = 'Text snippet from the article: ' + data.sentence_list[100].text
 }
 
 const clearUI = () => {
@@ -12,6 +13,7 @@ const clearUI = () => {
     document.getElementById('subjectivity').innerHTML = ''
     document.getElementById('confidence').innerHTML = ''
     document.getElementById('irony').innerHTML = ''
+    document.getElementById('text-snippet').innerHTML = ''
 }
 
 function handleSubmit(event) {
